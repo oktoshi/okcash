33fd863e1d4fd66181b7d39a0c0906922a2bdfc7
oktoshi <devteam@okcash.co>
2024-12-20 17:38:19 -0600

    eslint flat config

--- eslint.config.js
+++ eslint.config.js
@@ -2,17 +2,8 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
-import { FlatCompat } from '@eslint/eslintrc';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended
-});
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
 
 export default [
   {
@@ -21,44 +12,33 @@ export default [
     languageOptions: {
       ecmaVersion: 2020,
       sourceType: 'module',
-      globals: {
-        ...globals.browser,
-        ...globals.es2020
-      },
+      parser: tsparser,
       parserOptions: {
         ecmaFeatures: {
           jsx: true
         }
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.es2020
       }
     },
-    linterOptions: {
-      reportUnusedDisableDirectives: true,
-      noInlineConfig: false
-    },
     plugins: {
+      '@typescript-eslint': tseslint,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh
     },
     rules: {
       ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true }
       ],
-      'no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
       'no-console': ['warn', { allow: ['warn', 'error'] }]
     }
-  },
-  ...compat.config({
-    extends: [
-      'plugin:@typescript-eslint/recommended'
-    ],
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    rules: {
-      '@typescript-eslint/no-unused-vars': ['warn'],
-      '@typescript-eslint/no-explicit-any': ['warn']
-    }
-  })
+  }
 ];
